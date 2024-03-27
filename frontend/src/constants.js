7a49bce7970baeb7af8b1924392008b05932b799
Mahelet,Kassa <maheletkassa1@gmail.com>
2024-03-27 00:26:53 -0400

    updating backend url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,5 +1,5 @@
 // export const BASE_URL =process.env.NODE_ENV === 'develeopment' ? 'http://localhost:5002' : '';
-export const BASE_URL = 'http://localhost:5002'; // If using proxy
+export const BASE_URL = 'https://ecommerce-mern-6tki.onrender.com'; // If using proxy
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
